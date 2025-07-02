397d854372ded3890fa4034c068fd34e986f3cf4
gabriel lima <ga97lima@gmail.com>
2025-07-02 19:44:09 -0300

    refatorando classe para pegar apenas os logs criticos do disco

--- scripts/disk_monitoring.py
+++ scripts/disk_monitoring.py
@@ -1,6 +1,6 @@
 from time import sleep
 
-from psutil import disk_usage, disk_io_counters
+from psutil import disk_usage
 
 from scripts.utils.logger import setup_logger
 from scripts.utils.format_out import format_bytes
@@ -10,82 +10,39 @@ from database.logs_repository import save_log_disk
 logger = setup_logger()
 
 
-class DiskStats:
-    def __init__(self):
-        pass
-
-    def get_total_disk(self):
-        return disk_usage('/').total
-
-    def get_free_disk(self):
-        return disk_usage('/').free
-    
-    def get_used_disk(self):
-        return disk_usage('/').used
-    
-    def get_percent_disk(self):
-        return disk_usage('/').percent
-    
-    def get_read_bytes(self):
-        return disk_io_counters().read_bytes
-
-    def get_write_bytes(self):
-        return disk_io_counters().write_bytes
-
-    def get_read_time(self):
-        return disk_io_counters().read_time
-
-    def get_write_time(self):
-        return disk_io_counters().write_time    
-
-
 class CriticalDiskDetector:
-    def __init__(self, threshold=80):
+    def __init__(self, threshold=60):
         self.threshold = threshold
 
-    def detect_critical_total(self, percent):
-        return percent > self.threshold
+    def get_critical_data_disk(self):
+        disk = disk_usage("/")
+        return {
+            "total": disk.total,
+            "used": disk.used,
+            "percent": disk.percent
+        }
     
-    def detect_critical_io(self, read_time, write_time):
-        return read_time > write_time
-
-    def detect_critical_io_bytes(self, read_bytes, write_bytes):
-        return read_bytes > write_bytes
-
+    def is_critical_disk(self):
+        return disk_usage("/").percent > self.threshold
+    
 class DiskMonitorController:
     def __init__(self, interval=2):
-        self.stats = DiskStats()
+        self.stats = CriticalDiskDetector()
         self.interval = interval
 
     def run(self):
         try:
             while True:
-                total = self.stats.get_total_disk()
-                free = self.stats.get_free_disk()
-                used = self.stats.get_used_disk()
-                percent = self.stats.get_percent_disk() 
-                read_time = self.stats.get_read_time()
-                write_time = self.stats.get_write_time()
-                write_bytes = self.stats.get_write_bytes()
-                read_bytes = self.stats.get_read_bytes()
+                data = self.stats.get_critical_data_disk()
+                logger.info(f"Disk total: {format_bytes(data['total'])}")
+                logger.info(f"Disk usada: {format_bytes(data['used'])}, uso: {data['percent']}%")
 
-                logger.info(f"Total Disk: {format_bytes(total)}")
-                logger.info(f"Free Disk: {format_bytes(free)}")
-                logger.info(f"Used Disk: {format_bytes(used)}") 
-                logger.info(f"Percent Disk usage: {percent}%")
-                logger.info("--" * 20)
-                logger.info(f"Read time: {read_time}")  
-                logger.info(f"Write time: {write_time}")
-                logger.info(f"Read bytes: {format_bytes(read_bytes)}")
-                logger.info(f"Write bytes: {format_bytes(write_bytes)}")
-                logger.info("--" * 20)
-                
-
-                if self.stats.get_percent_disk() > 80:
-                    logger.warning(f" Total Disk usage is high: {self.stats.get_percent_disk()}%")
-                    save_log_disk(total, used, percent)
+                if self.is_critical_disk():
+                    logger.warning(f"Uso critico de disco: {data['percent']}%")
+                    save_log_disk(data['total'], data['used'], data['percent'])
 
                 sleep(self.interval)
+
         except KeyboardInterrupt:
             logger.warning("Monitoramento interrompido pelo usuário.")
         except Exception as e:
