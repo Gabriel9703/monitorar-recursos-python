f60c65cdf790ea83021ab3d38a2871734bf81bd7
gabriel lima <ga97lima@gmail.com>
2025-07-02 19:45:05 -0300

    refatorando classe para pegar apenas os logs criticos da swap

--- scripts/swap_monitoring.py
+++ scripts/swap_monitoring.py
@@ -1,60 +1,46 @@
+from time import sleep
+
 from psutil import swap_memory
+
 from scripts.utils.logger import setup_logger
 from scripts.utils.format_out import format_bytes
 from database.logs_repository import save_log_swap
-from time import sleep
 
 logger = setup_logger()
-
-
-class SwapStats:
-    def __init__(self):
-        pass
-
-    def get_total_swap(self):
-        return swap_memory().total
-    
-    def get_free_swap(self):
-        return swap_memory().free
-    
-    def get_used_swap(self):
-        return swap_memory().used
-    
-    def get_percent_swap(self):
-        return swap_memory().percent
 
 
 class CriticalSwapDetector:
     def __init__(self, threshold=80):
         self.threshold = threshold
 
-    def detect_critical_total(self, usage):
-        return usage > self.threshold   
+    def get_critical_data_swap(self):
+        mem = swap_memory()
+        return {"total": mem.total, "used": mem.used, "percent": mem.percent}
+
+    def is_critical_swap(self):
+        return swap_memory().percent > self.threshold
+
 
 class SwapMonitorController:
     def __init__(self, interval=2):
-        self.stats = SwapStats()
         self.detector = CriticalSwapDetector()
-        self.interval = interval    
+        self.interval = interval
 
     def run(self):
         try:
             while True:
-                total = self.stats.get_total_swap()
-                free = self.stats.get_free_swap()
-                used = self.stats.get_used_swap()
-                percent = self.stats.get_percent_swap()
+                data = self.detector.get_critical_data_swap()
+                logger.info(f"Swap total: {format_bytes(data['total'])}")
+                logger.info(
+                    f"Swap usada: {format_bytes(data['used'])}, uso: {data['percent']}%"
+                )
 
-                logger.info(f"Total Swap: {format_bytes(total)}")
-                logger.info(f"Free Swap: {format_bytes(free)}")
-                logger.info(f"Used Swap: {format_bytes(used)}")
-                logger.info(f"Percent Swap usage: {percent}%")
-
-                if self.detector.detect_critical_total(self.stats.get_percent_swap()):
-                    logger.warning(f" Total Swap usage is high: {self.stats.get_percent_swap()}%")
-                    save_log_swap(total, used, percent)
+                if self.detector.is_critical_swap():
+                    logger.warning(f" Uso critico de Swap: {data['percent']}%")
+                    save_log_swap(data["total"], data["used"], data["percent"])
 
                 sleep(self.interval)
+
         except KeyboardInterrupt:
             logger.warning("Monitoramento interrompido pelo usuário.")
         except Exception as e:
