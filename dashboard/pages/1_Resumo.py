1e03d50c6b3a0fc981d34bec70b5c4f583bdaba6
gabriel lima <ga97lima@gmail.com>
2025-07-04 13:47:20 -0300

    rafatorando os imports

--- dashboard/pages/1_Resumo.py
+++ dashboard/pages/1_Resumo.py
@@ -1,19 +1,16 @@
-import random
-
 import streamlit as st
 from streamlit_autorefresh import st_autorefresh
 
-import plotly.graph_objects as go
-from metrics_static.cpu_info import CpuInfo
-from metrics_static.ram_info import RamInfo
-from metrics_static.network_info import NetworkInfo
-from metrics_static.disk_info import DiskInfo
-from metrics_static.swap_info import SwapInfo
+
+from dashboard.utils import donut_chart, area_chart
+from scripts.metrics_normal.cpu_info import CpuInfo
+from scripts.metrics_normal.ram_info import RamInfo
+from scripts.metrics_normal.network_info import NetworkInfo
+from scripts.metrics_normal.disk_info import DiskInfo
+from scripts.metrics_normal.swap_info import SwapInfo
 
 
 st_autorefresh(interval=2000, key="auto-refresh")
-
-
 
 info_cpu = CpuInfo()
 info_ram = RamInfo()
@@ -21,48 +18,10 @@ info_disk = DiskInfo()
 info_swap = SwapInfo()
 info_network = NetworkInfo()
 
-
-def donut_chart(label, percent, cor):
-    fig = go.Figure(go.Pie(
-        values=[percent, 100 - percent],
-        labels=["Usado", "Livre"],
-        hole=0.6,
-        marker_colors=[cor, "#e8e8e8"],
-        textinfo="none"
-    ))
-    fig.update_layout(
-        title={
-            "text": f"{label}: {percent:.1f}%",
-            "x": 0.5,
-            "xanchor": "center"
-        },
-        showlegend=False,
-        margin=dict(t=50, b=0, l=0, r=0),
-        height=200
-    )
-    return fig
-
-def area_chart(title, cor):
-    y = [random.uniform(30, 80) for _ in range(20)]
-    fig = go.Figure(go.Scatter(
-        y=y,
-        fill='tozeroy',
-        mode='lines',
-        line=dict(color=cor)
-    ))
-    fig.update_layout(
-        title=title,
-        margin=dict(t=30, b=0, l=0, r=0),
-        height=180,
-        xaxis=dict(visible=False),
-        yaxis=dict(visible=False),
-    )
-    return fig
-
 st.set_page_config(layout="wide")
+
 st.title("🖥️ Sistema de Monitoramento de Recursos")
 st.divider()
-
 
    
 col1, col2 = st.columns(2)
@@ -75,7 +34,6 @@ with col2:
     col7.plotly_chart(donut_chart("Disco", info_disk.get_percent_disk(), "#184022"), use_container_width=True)
     col8.plotly_chart(donut_chart("Swap", info_swap.get_percent_swap(),  "#0CED4C"), use_container_width=True)
 
-# Parte de baixo: gráfico de área com variação
 col3, col4 = st.columns(2, border=True)
 with col3:
     st.plotly_chart(area_chart("Variação de CPU", "#86CECF"), use_container_width=True)
