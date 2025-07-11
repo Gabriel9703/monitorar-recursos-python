cabcf1993c518c01a1522ffe0f609d5ce93d6172
gabriel lima <ga97lima@gmail.com>
2025-07-10 23:05:44 -0300

    ajustando o scripts main para assync

--- main.py
+++ main.py
@@ -4,17 +4,19 @@ from scripts.metrics_critical.network_monitoring import NetworkMonitorController
 from scripts.metrics_critical.ram_monitoring import RamMonitorController
 from scripts.metrics_critical.swap_monitoring import SwapMonitorController
 from scripts.metrics_critical.monitor_process import ProcessMonitorController
+from scripts.metrics_critical.disk_monitoring import DiskMonitorController
 from scripts.utils.logger import setup_logger
 
 logger = setup_logger()
 
 
 async def main():
+    disk_monitor = DiskMonitorController()
     processes = ProcessMonitorController()
     cpu_monitor = CpuMonitorController()
-    network_monitor = NetworkMonitorController(interval=2)
-    ram_monitor = RamMonitorController(interval=1.5) 
-    swap_monitor = SwapMonitorController(interval=2) 
+    network_monitor = NetworkMonitorController()
+    ram_monitor = RamMonitorController() 
+    swap_monitor = SwapMonitorController() 
 
     from scripts.utils.shared_metrics import SharedMetricsWriter
     metrics_write = SharedMetricsWriter()
@@ -30,6 +32,7 @@ async def main():
             ram_monitor.run(),
             swap_monitor.run(),
             processes.run(),
+            disk_monitor.run(),
             write_normal_metrics(),
 
         )
