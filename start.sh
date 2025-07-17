8dcee2e617a6b76788938796492c69668bd35977
gabriel lima <ga97lima@gmail.com>
2025-07-16 23:54:12 -0300

    config run do main

--- start.sh
+++ start.sh
@@ -3,5 +3,7 @@
 # Define o diretório raiz do projeto como PYTHONPATH
 export PYTHONPATH=$(pwd)
 
-# Roda o Streamlit a partir da raiz, apontando para o app dentro da pasta dashboard
-streamlit run /app/dashboard/Home.py --server.port=8501 --server.address=0.0.0.0
+
+#rodar o main.py da raiz
+
+python3 main.py
